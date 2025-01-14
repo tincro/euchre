482b8ddbe350007c3b5b54b0615656cd5d541c1d
Austin Cronin <austin.cronin3d@gmail.com>
2025-01-14 17:59:09 -0500

    added main interface barebones

---
+++ euchre/interface.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+"""The user interface for the app."""
+import sys
+
+from PySide6.QtCore import ( 
+    Qt,    
+)
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget
+)
+
+class MainInterface(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Python Euchre")
+        self.setMinimumHeight(480)
+        self.setMinimumWidth(960)
+
+        
+        # Welcome Title
+        label = QLabel("Welcome to the game of Euchre!")
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+
+        self.setCentralWidget(centralWidget)
+
+        
+        
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainInterface()
+    win.show()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
