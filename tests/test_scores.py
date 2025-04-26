b72db7a4e339b2864b93d3ce636aaa58eabbb383
Austin Cronin <austin.cronin3d@gmail.com>
2025-04-26 10:54:04 -0400

    Added tests directory

---
+++ tests/test_scores.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestScoreTrick(unittest.TestCase):
+    
+    def setUp(self):
+        return super().setUp()
+    
+    def test_getHighestRankingCard():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
